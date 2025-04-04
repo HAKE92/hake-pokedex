bbded599f676ec933b77aadd66c2609014815e95
Hamidou <hake92240@gmail.com>
2025-04-04 11:33:21 +0200

    bulbausur

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css'
 import PokemonCards from './components1/PokemonCards';
-
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 function App() {
+  
   return (
     <div>
-      <PokemonCards /> 
+      <PokemonCards pokemon={pokemonList[0]} /> 
     </div>
     
   );
--- src/components1/PokemonCards.tsx
+++ src/components1/PokemonCards.tsx
@@ -1,26 +1,17 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
-function PokemonCards () {
-const pokemon = pokemonList [1];
 
-    return (
-<figure>
-        {pokemon.imgSrc !== undefined ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>???</p>)}
-        
-        <figcaption>{pokemon.name}</figcaption>
-    </figure>
-    
-)
-    
+
+function PokemonCards({pokemon}) {
+
+
+  return (
+    <div>
+     <img src={pokemon.imgSrc} alt={pokemon.name} /> 
+
+     <figcaption>{pokemon.name}</figcaption>
+    </div>
+  );
+  // ...
 }
 
 
